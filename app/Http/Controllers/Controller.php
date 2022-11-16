64ceb3b94482282e51716b8bf1348909856546dd
KNobHack <akudandia7272@gmail.com>
2022-11-16 13:22:58 +0700

    Initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $viewFolder = '';
+
+    /**
+     * Get the evaluated view contents for the given view.
+     *
+     * @param  string|null  $view
+     * @param  \Illuminate\Contracts\Support\Arrayable|array  $data
+     * @param  array  $mergeData
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
+    public function view($view = 'index', $data = [], $mergeData = [])
+    {
+        $view =  rtrim($this->viewFolder, '/') . '/' . $view;
+        return view($view, $data, $mergeData);
+    }
+}
---
+++ resources/views/simpanan/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Simpanan</title>
+</head>
+
+<body>
+	<h1>Simpanan</h1>
+	<table>
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Nama</th>
+				<th>Nominal</th>
+				<th>Jenis</th>
+				<th>Tanggal</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php $no_urut = 1; ?>
+			<tr>
+				<td><?= $no_urut++ ?></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+
+</html>
