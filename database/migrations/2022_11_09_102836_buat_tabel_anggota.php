e89c655138dca63fdf1fc42baff23b672d990412
KNobHack <akudandia7272@gmail.com>
2022-11-30 16:47:40 +0700

    buat migrasi tabel angota

---
+++ database/migrations/2022_11_09_102836_buat_tabel_anggota.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anggota', function (Blueprint $table) {
+            $table->id('id');
+            $table->foreignId('user_id');
+            $table->string('nik', 16)->unique();
+            $table->string('nama');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->enum('agama', [
+                'Islam', 'Kristen', 'Katholik',
+                'Hindu', 'Budha', 'Konghucu'
+            ]);
+            $table->string('pekerjaan');
+            $table->text('alamat');
+            $table->string('nomor_telpon', 15)->unique();
+
+            $table->foreign('user_id')->on('users')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+};
